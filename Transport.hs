d69d548c287389f662ea6dca33c74ca86976d8ea
Kazu Yamamoto <kazu@iij.ad.jp>
2020-11-17 13:39:39 +0900

    adding test cases for server-only TPs.

--- Transport.hs
+++ Transport.hs
@@ -8,9 +8,12 @@ import Data.ByteString ()
 import System.Timeout
 import Test.Hspec
 import Network.TLS (AlertDescription(..))
+import Network.TLS.QUIC (ExtensionRaw)
 
 import Network.QUIC
 import Network.QUIC.Internal
+
+----------------------------------------------------------------
 
 runC :: ClientConfig -> (Connection -> IO a) -> IO (Maybe a)
 runC cc body = timeout 2000000 $ runQUICClient cc body
@@ -20,6 +23,18 @@ transportSpec cc0 = do
     describe "QUIC servers" $ do
         it "MUST send TRANSPORT_PARAMETER_ERROR if initial_source_connection_id is missing [Transport 7.3]" $ \_ -> do
             let cc = addHook cc0 $ setOnTransportParametersCreated dropInitialSourceConnectionId
+            runC cc waitEstablished `shouldThrow` check TransportParameterError
+        it "MUST send TRANSPORT_PARAMETER_ERROR if original_destination_connection_id is received [Transport 18.2]" $ \_ -> do
+            let cc = addHook cc0 $ setOnTransportParametersCreated setOriginalDestinationConnectionId
+            runC cc waitEstablished `shouldThrow` check TransportParameterError
+        it "MUST send TRANSPORT_PARAMETER_ERROR if preferred_address, is received [Transport 18.2]" $ \_ -> do
+            let cc = addHook cc0 $ setOnTransportParametersCreated setPreferredAddress
+            runC cc waitEstablished `shouldThrow` check TransportParameterError
+        it "MUST send TRANSPORT_PARAMETER_ERROR if retry_source_connection_id is received [Transport 18.2]" $ \_ -> do
+            let cc = addHook cc0 $ setOnTransportParametersCreated setRetrySourceConnectionId
+            runC cc waitEstablished `shouldThrow` check TransportParameterError
+        it "MUST send TRANSPORT_PARAMETER_ERROR if stateless_reset_token is received [Transport 18.2]" $ \_ -> do
+            let cc = addHook cc0 $ setOnTransportParametersCreated setStatelessResetToken
             runC cc waitEstablished `shouldThrow` check TransportParameterError
         it "MUST send TRANSPORT_PARAMETER_ERROR if max_udp_payload_size is invalid [Transport 7.4 and 18.2]" $ \_ -> do
             let cc = addHook cc0 $ setOnTransportParametersCreated setMaxUdpPayloadSize
@@ -36,9 +51,14 @@ transportSpec cc0 = do
         it "MUST send PROTOCOL_VIOLATION if reserved bits in Short are non-zero [Transport 17.2]" $ \_ -> do
             let cc = addHook cc0 $ setOnPlainCreated $ rrBits RTT1Level
             runC cc waitEstablished `shouldThrow` check ProtocolViolation
-        it "MUST send no_application_protocol TLS alert if no application protocols are supported" $ \_ -> do
+        it "MUST send no_application_protocol TLS alert if no application protocols are supported [TLS 8.1]" $ \_ -> do
             let cc = cc0 { ccALPN = \_ -> return $ Just ["dummy"] }
             runC cc waitEstablished `shouldThrow` check (CryptoError NoApplicationProtocol)
+        it "MUST the send missing_extension TLS alert if the quic_transport_parameters extension does not included [TLS 8.2]" $ \_ -> do
+            let cc = addHook cc0 $ setOnTLSExtensionCreated (const [])
+            runC cc waitEstablished `shouldThrow` check (CryptoError MissingExtension)
+
+----------------------------------------------------------------
 
 addHook :: ClientConfig -> (Hooks -> Hooks) -> ClientConfig
 addHook cc modify = cc'
@@ -52,6 +72,14 @@ addHook cc modify = cc'
 setOnPlainCreated :: (EncryptionLevel -> Plain -> Plain) -> Hooks -> Hooks
 setOnPlainCreated f hooks = hooks { onPlainCreated = f }
 
+setOnTransportParametersCreated :: (Parameters -> Parameters) -> Hooks -> Hooks
+setOnTransportParametersCreated f hooks = hooks { onTransportParametersCreated = f }
+
+setOnTLSExtensionCreated :: ([ExtensionRaw] -> [ExtensionRaw]) -> Hooks -> Hooks
+setOnTLSExtensionCreated f params = params { onTLSExtensionCreated = f }
+
+----------------------------------------------------------------
+
 rrBits :: EncryptionLevel -> EncryptionLevel -> Plain -> Plain
 rrBits lvl0 lvl plain
   | lvl0 == lvl = if plainPacketNumber plain /= 0 then
@@ -60,11 +88,23 @@ rrBits lvl0 lvl plain
                     plain
   | otherwise   = plain
 
-setOnTransportParametersCreated :: (Parameters -> Parameters) -> Hooks -> Hooks
-setOnTransportParametersCreated f hooks = hooks { onTransportParametersCreated = f }
-
 dropInitialSourceConnectionId :: Parameters -> Parameters
 dropInitialSourceConnectionId params = params { initialSourceConnectionId = Nothing }
+
+dummyCID :: Maybe CID
+dummyCID = Just $ toCID "DUMMY"
+
+setOriginalDestinationConnectionId :: Parameters -> Parameters
+setOriginalDestinationConnectionId params = params { originalDestinationConnectionId = dummyCID }
+
+setPreferredAddress :: Parameters -> Parameters
+setPreferredAddress params = params { preferredAddress = Just "DUMMY" }
+
+setRetrySourceConnectionId :: Parameters -> Parameters
+setRetrySourceConnectionId params = params { retrySourceConnectionId = dummyCID }
+
+setStatelessResetToken :: Parameters -> Parameters
+setStatelessResetToken params = params { statelessResetToken = Just $ StatelessResetToken "DUMMY" }
 
 setMaxUdpPayloadSize :: Parameters -> Parameters
 setMaxUdpPayloadSize params = params { maxUdpPayloadSize = 1090 }
@@ -73,6 +113,8 @@ unknownFrame :: EncryptionLevel -> EncryptionLevel -> Plain -> Plain
 unknownFrame lvl0 lvl plain
   | lvl0 == lvl = plain { plainFrames = UnknownFrame 0x20 : plainFrames plain }
   | otherwise   = plain
+
+----------------------------------------------------------------
 
 check :: TransportError -> QUICError -> Bool
 check te (TransportErrorOccurs te' _) = te == te'
